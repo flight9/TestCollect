42259dde2548ccb71b00d9a721ff7965d14751f2
flight9 <ealeaf@sohu.com>
2017-11-08 10:04:36 +0800

    add photo_cordova.vue for common use

---
+++ src/components/tri_component/photo_cordova.vue
@@ -0,0 +1,70 @@
+<template>
+  <q-btn @click="takephoto" :small="small">
+    <q-icon name="camera_alt" />
+  </q-btn>
+</template>
+
+<script>
+  /* global Camera */
+  /* eslint no-undef: "error" */
+  import {
+    QBtn,
+    QIcon
+  } from 'quasar'
+  export default {
+    component: {
+      QBtn,
+      QIcon
+    },
+    props: {
+      small: {
+        type: Boolean,
+        default: true
+      },
+      options: {
+        type: Object,
+        default  () {
+          return {
+            height: 720,
+            width: 540
+          }
+        }
+      }
+    },
+    data () {
+      return {}
+    },
+    methods: {
+      takephoto () {
+        return new Promise((resolve, reject) => {
+          var onSuccess = (imageURI) => {
+            this.$emit('success', imageURI)
+            resolve(imageURI)
+          }
+
+          var onFail = (message) => {
+            if (message === 'Camera cancelled.') {
+              return
+            }
+            alert('Failed because: ' + message)
+            this.$emit('fail', message)
+            reject(message)
+          }
+
+          var options = {
+            quality: 50,
+            destinationType: Camera.DestinationType.FILE_URI,
+            targetHeight: this.options.height,
+            targetWidth: this.options.width,
+            correctOrientation: true
+          }
+
+          navigator.camera.getPicture(onSuccess, onFail, options)
+        })
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
